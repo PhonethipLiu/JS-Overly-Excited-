d5261397c2b88bce6441299c8a9ec09053af23e0
Phonethip <phonethip.liu@gmail.com>
2018-02-18 23:19:48 -0600

    basic working option 1

--- overly-excited.js
+++ overly-excited.js
@@ -10,21 +10,44 @@ let sentence = ["The", "walrus", "danced", "through", "the", "trees", "in", "the
     The addExcitement function should be an impure function, and accept the array as the sole argument. It should iterate over the array
     and output the words to the browser console.
 */
-function addExcitement (theWordArray) {
+function addExcitement () {
     let wordArray = [];
     for (var i = 0; i < sentence.length; i++){
-        // if (i = 3)
+        if (i % 3 > 1) {
+            sentence[i] += "!";  
+        }
         console.log(wordArray += sentence[i] + " ");
     }
-}
+};
 
 // Invoke the function and pass in the array
 addExcitement(sentence);
+//addExcitement(sentence);
+//addExcitement(sentence);
 
 
 
-// ============= Adding Conditions to the Loop =========//
 
+
+
+// ============= Even More Excited =========//
+
+// Add logic in the addExcitement function to the increase the number of exclamation points after every third word. The number of exclamation points (!) will be determined by how many times the counter variable can be divided by 3.
+
+// Example output:
+
+// The
+// The walrus
+// The walrus danced!
+// The walrus danced! through
+// The walrus danced! through the
+// The walrus danced! through the trees!!
+// The walrus danced! through the trees!! in
+// The walrus danced! through the trees!! in the
+// The walrus danced! through the trees!! in the light!!!
+// The walrus danced! through the trees!! in the light!!! of
+// The walrus danced! through the trees!! in the light!!! of the
+// The walrus danced! through the trees!! in the light!!! of the moon!!!!
 
 
 
