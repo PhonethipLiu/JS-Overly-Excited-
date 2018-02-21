8b2fc759e183ab0f61ade94d0cea9d3480b391ac
Phonethip <phonethip.liu@gmail.com>
2018-02-21 03:34:15 -0600

    Exercised is finally completed

--- overly-excited.js
+++ overly-excited.js
@@ -6,55 +6,38 @@ console.log("JS Overly excited is working");
 // Create an array that contains the words in the sentence
 let sentence = ["The", "walrus", "danced", "through", "the", "trees", "in", "the", "light", "of", "the", "moon"];
 
+let nextSentence = ["I", "am", "writing", "coding", "an", "array", "of", "words", "for", "a", "new", "sentence"];
 /*
     The addExcitement function should be an impure function, and accept the array as the sole argument. It should iterate over the array
     and output the words to the browser console.
 */
-let wordArray =[];
+let markedUpSentence = "";
 
-function addExcitement () {
+// fat arrow time
+let addExcitement = (array, punctuation) => {
     
-    for (var i = 0; i < sentence.length; i++){ //loops thru sentence array
-        wordArray += " " + sentence[i]; //add the wordArray a space and index item from sentence array
-        console.log(wordArray); // this displays the looped words in sentence array
+    for (var i = 0; i < array.length; i++){ //loops thru an array
+        markedUpSentence += " " + array[i]; //add to the markedUpSentence a space and index item from sentence array
+        console.log(markedUpSentence); // this displays the looped words in sentence array
+        document.getElementById("excited").innerHTML += `${markedUpSentence} <br>`;
 
-        // ***** Even More Excited ****//
+        // ***** Even More puncs ****//
         if (i % 3 > 1) { // conditions for the loop; if the remainder of index item is greater than 1 
             let x = i / 3; // and the index is divisible by 3 
             for (var j = 0; j < x; j++) // loop for this condition
-            wordArray += "!"; // then add an ! to the wordArray
+            markedUpSentence += punctuation; // then add an punctuation to the markedUpSentence
         } 
     }
-     console.log(wordArray); // this displays the looped words in sentence array with the added ! when the conditions are met
-}
-
+     console.log(markedUpSentence); // this displays the looped words in sentence array with the added punctuation when the conditions are met
+    document.getElementById("excited").innerHTML += `${markedUpSentence}<br><br>`;
+};
 
 // Invoke the function and pass in the array
-addExcitement(sentence);
 
+//addExcitement(sentence,"?");
 
+addExcitement(nextSentence,"&");
 
-
-
-
-// ============= Even More Excited =========//
-
-// Add logic in the addExcitement function to the increase the number of exclamation points after every third word. The number of exclamation points (!) will be determined by how many times the counter variable can be divided by 3.
-
-// Example output:
-
-// The
-// The walrus
-// The walrus danced!
-// The walrus danced! through
-// The walrus danced! through the
-// The walrus danced! through the trees!!
-// The walrus danced! through the trees!! in
-// The walrus danced! through the trees!! in the
-// The walrus danced! through the trees!! in the light!!!
-// The walrus danced! through the trees!! in the light!!! of
-// The walrus danced! through the trees!! in the light!!! of the
-// The walrus danced! through the trees!! in the light!!! of the moon!!!!
 
 
 
